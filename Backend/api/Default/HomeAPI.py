47bd7bdb3db83409659ac35847c5635891373b0b
RiwRiwara <awirut2629@gmail.com>
2023-10-29 22:02:55 +0700

    This template point

--- Backend/api/Default/HomeAPI.py
+++ Backend/api/Default/HomeAPI.py
@@ -11,3 +11,40 @@ def api_entry():
         'collection_names': collection_names
     }
     return jsonify(response)
+
+
+@defaultAPI.route('/')
+def index():
+    if 'IsUserLoggedIn' not in session:
+        session['IsUserLoggedIn'] = False
+
+    if session['IsUserLoggedIn']:
+        return redirect(url_for('defaultAPI.page', page='menu'))
+
+    return render_template('index.html', IsUserLoggedIn=session['IsUserLoggedIn'], UserType=session.get('UserType', None), pageData=None)
+
+
+@defaultAPI.route('/view_session')
+def view_session():
+    session_data = dict(session)
+    return jsonify(session_data)
+
+@defaultAPI.route('/page/<page>')
+def page(page):
+    if 'IsUserLoggedIn' not in session:
+        session['IsUserLoggedIn'] = False
+    if 'UserType' not in session:
+        session['UserType'] = "user"
+
+    if not session['IsUserLoggedIn'] and page not in ['login', 'signup']:
+        return redirect(url_for('defaultAPI.index'))
+    if session['IsUserLoggedIn'] and page in ['login', 'signup']:
+        return redirect(url_for('defaultAPI.index'))
+    if session['UserType'] == 'user' and page in ['admin']:
+        return redirect(url_for('defaultAPI.index'))
+
+    return renderPage(page, request.args)
+
+def renderPage(page, args):
+    pageData = {}
+    return render_template(page + '.html', IsUserLoggedIn=session['IsUserLoggedIn'], UserType=session.get('UserType', None), pageData=pageData)
